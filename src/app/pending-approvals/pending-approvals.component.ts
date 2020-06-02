6dbaea261788f1bf1f08dbcd083964a6ce7b33a9
Balaji Venkata <Balaji.Venkata@marlabs.com>
2020-06-02 17:47:29 +0530

    production issues are fixed

--- src/app/pending-approvals/pending-approvals.component.ts
+++ src/app/pending-approvals/pending-approvals.component.ts
@@ -2,29 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../home/home.service';
 
 @Component({
-  selector: 'app-pending-approvals',
-  templateUrl: './pending-approvals.component.html',
-  styleUrls: ['./pending-approvals.component.scss']
+    selector: 'app-pending-approvals',
+    templateUrl: './pending-approvals.component.html',
+    styleUrls: ['./pending-approvals.component.scss']
 })
 export class PendingApprovalsComponent implements OnInit {
-  
-  isDashboardCollapsed: boolean = true;
-  _sidebarExpansionSubscription: any = null;
 
-  constructor(private _homeService: HomeService) { }
+    isDashboardCollapsed: boolean = true;
+    _sidebarExpansionSubscription: any = null;
 
-  ngOnDestroy() {
-    if (this._sidebarExpansionSubscription) {
-      this._sidebarExpansionSubscription.unsubscribe();
+    constructor(private _homeService: HomeService) { }
+
+    ngOnDestroy() {
+        if (this._sidebarExpansionSubscription) {
+            this._sidebarExpansionSubscription.unsubscribe();
+        }
     }
-  }
-  
-  ngOnInit() {
-    this.isDashboardCollapsed = true;
 
-    this._sidebarExpansionSubscription = this._homeService.isSidebarCollapsed.subscribe(data => {
-      this.isDashboardCollapsed = !data;
-    });
-  }
+    ngOnInit() {
+        this.isDashboardCollapsed = true;
+
+        this._sidebarExpansionSubscription = this._homeService.isSidebarCollapsed.subscribe(data => {
+            this.isDashboardCollapsed = !data;
+        });
+    }
 
 }
