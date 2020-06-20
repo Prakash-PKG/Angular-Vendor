fd2d5bd3663c77702bc49ed8112104e502a99a68
Khadija Bohra <Khadija.Bohra@marlabs.com>
2020-06-20 16:44:32 +0530

    backdrop

--- src/app/vendor-documents/vendor-documents.component.ts
+++ src/app/vendor-documents/vendor-documents.component.ts
@@ -129,11 +129,11 @@ export class VendorDocumentsComponent implements OnInit {
     }
     onAttachFileClick(documentTypeId: number) {
         let filesReq: VendorDocumentReqModel = {
-            userId: '106994',
+            // userId: '106994',
             fileDetails: this.filesMap[documentTypeId].filesList,
-            vendorMasterId: 166
-            // userId: globalConstant.userDetails.isVendor ? globalConstant.userDetails.userEmail : globalConstant.userDetails.userId,
-            // vendorMasterId: this._appService.vendorRegistrationDetails.vendorMasterId
+            // vendorMasterId: 166
+            userId: globalConstant.userDetails.isVendor ? globalConstant.userDetails.userEmail : globalConstant.userDetails.userId,
+            vendorMasterId: this._appService.vendorRegistrationDetails.vendorMasterId
         }
         this._vendorRegistrationService.uploadVendorDocuments(filesReq)
             .subscribe(response => {
