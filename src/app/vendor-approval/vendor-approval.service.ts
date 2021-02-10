3c6b4b9c650ccfbf7bdb5394a79254765643486e
Khadija Bohra <Khadija.Bohra@marlabs.com>
2021-02-10 14:42:32 +0530

    remove edit functionality

--- src/app/vendor-approval/vendor-approval.service.ts
+++ src/app/vendor-approval/vendor-approval.service.ts
@@ -45,32 +45,17 @@ export class VendorApprovalService {
             detailsModel.vendorMasterDocumentVOList = data["vendorMasterDocumentVOList"];
             detailsModel.countriesList = data["countryDataVOList"];
             detailsModel.regionMasterVOList = data["regionMasterVOList"];
-            detailsModel.bankAccountTypeList = data["bankAccountTypeVOList"];
+            detailsModel.bankAccountTypeList =data["bankAccountTypeVOList"];
         }
+
         return detailsModel;
     }
 
-    async getExVendorInitData(vendorMasterId: number) {
-        let url = this._appService.baseUrl + 'fileDetails/' + vendorMasterId
-        try {
-            let response = await this._http.get(url, { responseType: 'json', observe: 'response' }).toPromise();
-            return this.prepareVendorRegistrationInitData(response.body);
-        } catch (error) {
-            await console.log(error);
-            return (new VendorApprovalInitResultModel());
-        }
-    }
-
-    updateVendorApprovalDetails(updateReqModel: VendorApprovalReqModel, isExistingVendor: boolean) {
-        let url: string = '';
-        if (isExistingVendor)
-            url = this._appService.baseUrl + "updateVendorDetails";
-        else
-            url = this._appService.baseUrl + "updateVendorApproval";
+    updateVendorApprovalDetails(updateReqModel: VendorApprovalReqModel) {
+        let url = this._appService.baseUrl + "updateVendorApproval";
         return this._http.post(url, updateReqModel, { responseType: 'json', observe: 'response' });
     }
-
-
+    
     sendBackForCorrection(sendVendCorrId: VendorRegistrationDetailRequestModel) {
         let url = this._appService.baseUrl + "fetchVendor";
         return this._http.post(url, sendVendCorrId, { responseType: 'json', observe: 'response' });
