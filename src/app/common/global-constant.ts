975fb50a92da06d4194a64e284f0c6f52ae9bec2
Soma Aravinda Sai Kumar Reddy <Soma.Reddy@marlabs.com>
2021-02-12 19:23:16 +0530

    added new us purchage groups in global constant

--- src/app/common/global-constant.ts
+++ src/app/common/global-constant.ts
@@ -6,10 +6,12 @@ export const countryCompanyCodes: any = {
 
 export const globalConstant : any = {
     poRoles: ["adp", "hrp", "isp", "itp", "lgp", "mkp", 'fip', 'psp', 'cgp', 'ssp', 'isp', 'hr1', 'hr2', 'hr3', 'hr4', 'ism',
-                "adp-us"],
+                "adp-us","cgp-us","fip-us","hr1-us","hr2-us","hr3-us","hr4-us","ism-us","isp-us","itp-us","lgp-us","mkp-us"
+                ,"psp-us","ssp-us"],
     invUploadRoles: ["adp-upload", "hrp-upload", "isp-upload", "itp-upload", "lgp-upload", "mkp-upload", "fip-upload",
                         "psp-upload", "cgp-upload", "ssp-upload", "hr1-upload", "hr2-upload", "hr3-upload", "hr4-upload", "ism-upload",
-                        "adp-upload-us"],
+                        "adp-upload-us","cgp-upload-us","hr1-upload-us","hr2-upload-us","hr3-upload-us","hr4-upload-us"
+                        ,"ism-upload-us","isp-upload-us","itp-upload-us","lgp-upload-us","mkp-upload-us","psp-upload-us","ssp-upload-us"],
     invSubContractReceiverRoles: ["hrp-receiver"],
     functionalHeadRoles: ["functional_head"],
     financeRoles: ["finance"],
